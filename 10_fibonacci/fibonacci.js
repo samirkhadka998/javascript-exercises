3a2d70062fcb18481fb7a2067c807e5ff21640c0
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-06 15:13:53 +0545

    Calculated fibonacciSequence
    Task 10 was difficult there are other easy ways to do it

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num < 0 || num * 1 == NaN) {
+        return "OOPS";
+    }
+
+    num = Number(num);
+    let fibonacciSequence = [1, 1];
+
+    for (let i = 2; i < num; i++) {
+        let lastNum = fibonacciSequence[fibonacciSequence.length - 1];
+        let secondLastNum = fibonacciSequence[fibonacciSequence.length - 2];
+        fibonacciSequence.push(lastNum + secondLastNum);
+
+    }
+
+    return fibonacciSequence[fibonacciSequence.length - 1];
 
 };
 
