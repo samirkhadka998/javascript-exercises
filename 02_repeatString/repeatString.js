62163021a67a533b87bc5fb18bfedd33e26d2b54
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-06 09:15:08 +0545

    Repeated string n number of times
    Task was to have parameter string and int and repeat string int number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(repeatingValue, repeatingTime) {
+    if(repeatingTime < 0){
+        return 'ERROR';
+    }
+    let totalRepeatingValue = '';
+    for(let i = 0; i< repeatingTime; i++){
+        totalRepeatingValue+=repeatingValue;
+    }
+    return totalRepeatingValue;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+    
