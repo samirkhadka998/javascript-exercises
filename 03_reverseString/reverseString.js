9ae3baac26efc22cc4591dcc5e52dbb9f11e6c63
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-06 10:43:10 +0545

    Reversed string using for loop , we can also use array split and reverse
    Requirement for 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverseText = "";
+    for(let i = text.length -1; i >=0; i--){
+        reverseText+=text[i];
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
