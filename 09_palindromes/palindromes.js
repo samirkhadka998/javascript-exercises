4abdf0cdbb14171c5d679f5b57d6b0dfe0e1fe69
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-06 14:28:43 +0545

    Find out palindromes words, same word from left to right , regex to remove other character than number alpha character

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    // Regex to only include number and alphabet and make lowercase
+    text = text.replace(/\W/g,'').toLowerCase();
+    let reverseText = '';
+    for (let i = text.length -1 ; i >= 0; i--) {
+        reverseText += text[i];
+    }
+    return text === reverseText;
 };
+
+function isLetter(char){
+    return char.toLowerCase() != char.toUpperCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
