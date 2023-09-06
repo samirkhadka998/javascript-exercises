083a9f2223cd89aaef566dfe9a323da450f712de
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-06 11:54:00 +0545

    Added number from minimum to maximum
    Show error on negative or non numeric number , task of o5 sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0 || typeof(num1) != 'number' ||  typeof(num2) != 'number'){
+        return "ERROR";
+    }
+    let minNumber = Math.min(num1,num2);
+    let maxNumber = Math.max(num1, num2);
+    let sum = 0;
 
+    for(let i = minNumber ; i <= maxNumber; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
